de0f7ed6444335c95d48ff39d31f2e87c05e4a4e
void-kun <hoangzrik@gmail.com>
2024-01-14 17:48:37 +0700

    init

---
+++ algorithms/generation/permutation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+template <typename T> std::vector<T> permutation(std::vector<T> sInput) {
+  std::vector<T> result(0, sInput.size());
+  return result;
+}
+
+int main() {
+  std::cout << "hello world! \n";
+  return 0;
+}
